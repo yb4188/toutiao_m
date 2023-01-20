44eba5bc0824c3e1c177d836d4f62336a42dcf2a
YouBing <2900856508@qq.com>
2023-01-20 20:55:56 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
+<!--
+ * @Author: YouBing 2900856508@qq.com
+ * @Date: 2023-01-20 20:50:30
+ * @LastEditors: YouBing 2900856508@qq.com
+ * @LastEditTime: 2023-01-20 20:53:59
+ * @FilePath: \toutiao_m\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
